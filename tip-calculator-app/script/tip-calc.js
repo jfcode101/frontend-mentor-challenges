ffee0985757b79f48ef86a36b942204960be60d1
jfcode101 <125421939+jfcode101@users.noreply.github.com>
2024-02-19 12:38:57 -0500

    modification and adding files add images modify index.html, tip-calc.js, s.css and README.md

--- tip-calculator-app/script/tip-calc.js
+++ tip-calculator-app/script/tip-calc.js
@@ -1,130 +1,113 @@
-// creating variables
-const input = document.querySelectorAll("input");
 const billIn = document.querySelector("#bill-input");
 const btnTips = document.querySelectorAll(".btn-tip");
-const customIn = document.querySelector("#custom-tip");
+const customTipIn = document.querySelector("#custom-tip");
 const peopleIn = document.querySelector("#people-input");
-const tipPerPerson = document.querySelector("#tip-amount");
-const totalPerPerson = document.querySelector("#total-amount");
+const tipSingle = document.querySelector("#tip-amount");
+const totalSingle = document.querySelector("#total-amount");
 const btnReset = document.querySelector(".btn-reset");
-
 const titleRight = document.querySelectorAll(".title-right");
 
-// other variable
-let billVal = 0, // stores the bill input value
-  customVal = 0, // stores the value of customVal input value
-  numOfPeopleVal = 0, // stores the value of number of people input value
-  btnVal = 0; // stores the value of the clicked button
+// variable
+let billVal = 0,
+  customTipVal = 0,
+  peopleVal = 0,
+  btnTipVal = 0;
 
-// reset the form
+// populate default value
+tipSingle.innerHTML = `$0.00`;
+totalSingle.innerHTML = `$0.00`;
+
 btnReset.addEventListener("click", (e) => {
   e.preventDefault();
-  tipPerPerson.innerText = "$0.00";
-  totalPerPerson.innerText = "$0.00";
+  calculate();
+  tipSingle.innerHTML = `$0.00`;
+  totalSingle.innerHTML = `$0.00`;
+  customTipIn.value = "";
 
-  for (const inp of input) {
+  const inputs = [billIn, peopleIn];
+  for (const inp of inputs) {
     inp.value = 0;
-    input[1].value = "";
     inp.style.color = "hsl(184, 14%, 56%)";
-    input[1].style.color = "hsl(183, 100%, 15%)";
   }
+  billVal = 0;
+  peopleVal = 0;
+
+  btnTips.forEach((btn) => {
+    btn.classList.remove("active");
+  });
 });
 
-// get the value of the bill input
-billIn.addEventListener("input", (e) => {
-  //   e.preventDefault();
-
-  checkInput(e);
-  e.target.style.color = "hsl(183, 100%, 15%)";
-  billVal = Number(e.target.value);
-  console.log(billVal);
-
-  //
+// adding event listeners
+billIn.addEventListener("input", () => {
+  checkInput();
+  billVal = Number(parseFloat(billIn.value));
   calculate();
-
-  //
 });
 
-// get the value of custom tip input
-customIn.addEventListener("input", (e) => {
-  customVal = Number(e.target.value);
-  console.log(customVal);
-  //
-  calculate();
-
-  //
-});
-
-// get the value of number of people
-peopleIn.addEventListener("input", (e) => {
-  //   e.preventDefault();
-
-  checkInput(e);
-  e.target.style.color = "hsl(183, 100%, 15%)";
-  numOfPeopleVal = Number(e.target.value);
-  console.log(numOfPeopleVal);
-
-  //
-  calculate();
-
-  //
-});
-
-// get the value of the clicked button value
 btnTips.forEach((btn) => {
   btn.addEventListener("click", (e) => {
-    btnVal = Number(btn.value);
-    console.log(btnVal);
+    e.preventDefault();
+    for (let b = 0; b < btnTips.length; b++) {
+      btnTips[b].classList.remove("active");
+    }
+    e.target.classList.add("active");
+    btnTipVal = Number(parseFloat(btn.value)) / 100;
+    if (e.target.classList.contains("active")) {
+      customTipVal = 0;
+      customTipIn.innerHTML = "0";
+      btnTipVal = Number(parseFloat(btn.value)) / 100;
+      calculate();
+    }
   });
-
-  //
-  calculate();
-
-  //
 });
 
-// check if input is not empty
-function checkInput(e) {
-  for (let i = 0; i < input.length; i++) {
-    if (e.target.value === "" || Number(e.target.value) === 0) {
-      e.target.style.outline = "2px solid rgb(197, 126, 34)";
+customTipIn.addEventListener("input", () => {
+  customTipVal = Number(parseFloat(customTipIn.value)) / 100;
+  btnTips.forEach((btn) => {
+    btn.classList.remove("active");
+  });
+  calculate();
+});
+
+peopleIn.addEventListener("input", () => {
+  checkInput();
+  peopleVal = Number(parseInt(peopleIn.value));
+  calculate();
+});
+
+// function to check whether an input is a valid number or not
+// Bill and Number of people
+function checkInput() {
+  const inputs = [billIn, peopleIn];
+  let focusedInput = document.activeElement;
+  for (let i = 0; i < inputs.length; i++) {
+    if (
+      focusedInput.value === "" ||
+      Number(focusedInput.value) === 0 ||
+      Number(focusedInput.value) < 0
+    ) {
+      focusedInput.style.outline = "2px solid rgb(197, 126, 34)";
       titleRight[1].style.display = "block";
     } else {
-      e.target.style.outline = "2px solid hsl(172, 67%, 45%)";
-
+      focusedInput.style.outline = "2px solid hsl(172, 67%, 45%)";
       titleRight[1].style.display = "none";
     }
   }
 }
 
-// function to execute calculation
+// function that calculate an individual tip and total payments
+// and then populates their appropriate
 function calculate() {
-  let totalBill = 0,
-    totalTip = 0,
-    totalSingle = 0,
-    tiplSingle = 0;
-
-  if (customIn.value === "" || customVal === 0) {
-    totalTip = btnVal / 100;
-
-    console.log(totalTip);
-
-    totalBill = billVal * (1 + totalTip);
-
-    tiplSingle = (billVal * totalTip) / numOfPeopleVal;
-
-    totalSingle = totalBill / numOfPeopleVal;
-
-    console.log(totalBill);
+  let tipSingleVal = 0,
+    totalSingleVal = 0;
+  if (customTipIn.value === "" || customTipVal === 0) {
+    tipSingleVal = (billVal * btnTipVal) / peopleVal;
+    // totalSingleVal = billVal / peopleVal + tipSingleVal;
   } else {
-    totalTip = customVal;
-    console.log(totalTip);
-
-    totalBill = billVal + totalTip;
-    console.log(totalBill);
-    tiplSingle = totalTip / numOfPeopleVal;
+    tipSingleVal = (billVal * customTipVal) / peopleVal;
   }
+  totalSingleVal = billVal / peopleVal + tipSingleVal;
 
-  tipPerPerson.innerText = `$${tiplSingle.toFixed(2)}`;
-  totalPerPerson.innerText = `$${totalSingle.toFixed(2)}`;
+  tipSingle.innerHTML = `$${tipSingleVal.toFixed(2)}`;
+  totalSingle.innerHTML = `$${totalSingleVal.toFixed(2)}`;
 }
